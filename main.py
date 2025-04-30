4c89a3a4e8c03799168d2e78eada9b82ce3c2d44
wbeeker <willbeeker@gmail.com>
2025-04-30 13:03:42 -0700

    Added 'https' formatting, enable JavaScript, updated custom user-agent

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
 
+class CustomWebPage(QWebEnginePage):
+    def userAgentForUrl(self, url):
+        return (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/122.0.0.0 Safari/537.36"
+        )
 
 class MyWebBrowser(QMainWindow):
 
@@ -34,6 +41,8 @@ class MyWebBrowser(QMainWindow):
         nav_bar.addWidget(self.forward_btn)
 
         self.browser = QWebEngineView()
+        self.browser.setPage(CustomWebPage(self.browser))
+        self.browser.settings().setAttribute(QWebEngineSettings.JavascriptEnabled, True)
         self.browser.setUrl(QUrl("http://google.com"))
 
         self.go_btn.clicked.connect(lambda: self.navigate(self.url_bar.toPlainText()))
@@ -49,12 +58,13 @@ class MyWebBrowser(QMainWindow):
 
     def navigate(self, url):
         if not url.startswith("http"):
-            url = "http://" + url
+            url = "https://" + url
             self.url_bar.setText(url)
         self.browser.setUrl(QUrl(url))
 
     def update_url_bar(self, q):
         self.url_bar.setText(q.toString())
+
 
 if __name__ == "__main__":
     app = QApplication([])
